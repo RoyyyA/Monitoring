853078235755111d152dea0d974d45569d12d467
Roya Anooshe <42075518+RoyyyA@users.noreply.github.com>
2022-11-05 15:25:53 +0330

    Create my_container_check.sh

---
+++ Zabbix/my_container_check.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# checks if container "my_container" is up
+# same script will apply for my_container_2
+
+if [ "$(docker ps | grep -i my_container_1)" ] ; then
+echo "1"
+else
+echo "0"
+fi
+
