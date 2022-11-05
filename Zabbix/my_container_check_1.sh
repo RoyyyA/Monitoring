dc08a25c926ef86d1572de34a241879bd27fab12
Roya Anooshe <42075518+RoyyyA@users.noreply.github.com>
2022-11-05 15:27:48 +0330

    Update and rename my_container_check.sh to my_container_check_1.sh

--- Zabbix/my_container_check_1.sh
+++ Zabbix/my_container_check_1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # checks if container "my_container" is up
-# same script will apply for my_container_2
+# same script will apply for my_container_check_2
 
 if [ "$(docker ps | grep -i my_container_1)" ] ; then
 echo "1"
